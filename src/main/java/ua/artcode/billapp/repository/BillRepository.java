dc400a8cdc2b156ea5114a9c8f20207983031772
SmooZzzie <artemshchepets@gmail.com>
2017-12-06 11:58:04 +0200

    Done getOpenedBillsOfCompany task. - Added class CompanyServiceImpl. - Added Company Repository. - Changed type of field Provider in class Bill. - Added method getOpenedBills() in CompanyServiceImpl. - Added test for method getOpenedBills().

--- src/main/java/ua/artcode/billapp/repository/BillRepository.java
+++ src/main/java/ua/artcode/billapp/repository/BillRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ua.artcode.billapp.model.Bill;
 import ua.artcode.billapp.model.BillStatus;
+import ua.artcode.billapp.model.Company;
 import ua.artcode.billapp.model.Customer;
 
 import java.util.List;
@@ -12,8 +13,10 @@ import java.util.List;
  * Created by serhii on 03.12.17.
  */
 @Repository
-public interface BillRepository extends PagingAndSortingRepository<Bill,Long> {
+public interface BillRepository extends PagingAndSortingRepository<Bill, Long> {
 
     List<Bill> findByCustomerAndBillStatus(Customer customer, BillStatus billStatus);
+
+    List<Bill> findByProviderAndBillStatus(Company company, BillStatus billStatus);
 
 }
--- src/main/java/ua/artcode/billapp/service/CompanyService.java
+++ src/main/java/ua/artcode/billapp/service/CompanyService.java
@@ -1,7 +1,15 @@
 package ua.artcode.billapp.service;
 
+import ua.artcode.billapp.exception.AppException;
+import ua.artcode.billapp.model.Bill;
+import ua.artcode.billapp.model.Company;
+
+import java.util.List;
+
 /**
  * Created by serhii on 03.12.17.
  */
 public interface CompanyService {
+
+    List<Bill> getOpenedBills(Company company) throws AppException;
 }
