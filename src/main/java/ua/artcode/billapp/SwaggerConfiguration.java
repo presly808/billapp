e0fc1c6c54ab900fa35ae331861c3f7d19f7a09a
Sasha943 <MaslikSASHA1@gmail.com>
2017-12-10 11:50:53 +0200

    add swagger to project

---
+++ src/main/java/ua/artcode/billapp/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package ua.artcode.billapp;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+@EnableAutoConfiguration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ua.artcode.billapp.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "BillApp API",
+                "This API implemented by Artcode team",
+                "API 0.1",
+                "Api for BillApp",
+                new Contact("Artcode","art-code.com.ua", "artcodekiev@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
+
+
